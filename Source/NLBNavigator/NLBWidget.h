69f05fdbb2acf1469f5272247a23e971d7239051
Antokolos <antokolos@gmail.com>
2024-12-26 00:29:28 +0600

    NLBWidget

---
+++ Source/NLBNavigator/NLBWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NLBWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NLBNAVIGATOR_API UNLBWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
