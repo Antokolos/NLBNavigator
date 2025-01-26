b616cfd9a5e7c25088cefac1e9cf866090029d0f
Antokolos <antokolos@gmail.com>
2025-01-27 01:39:51 +0600

    PartialProgressData

---
+++ Source/NLBCore/Private/nlb/api/PartialProgressData.cpp
@@ -0,0 +1,35 @@
+#include "nlb/api/PartialProgressData.h"
+#include "nlb/api/ProgressData.h" // Actual header for ProgressData class
+
+PartialProgressData::PartialProgressData(
+    ProgressData* realProgressData,
+    int startingProgress,
+    int maximumAllowedProgress,
+    int itemsCountPerIncrement
+) : 
+    m_realProgressData(realProgressData),
+    m_startingProgress(startingProgress),
+    m_currentProgress(startingProgress),
+    m_maximumAllowedProgress(maximumAllowedProgress),
+    m_itemsCountPerIncrement(itemsCountPerIncrement),
+    m_currentItemsCount(0)
+{
+}
+
+void PartialProgressData::setRealProgressValue() {
+    m_currentItemsCount++;
+    if (m_currentItemsCount % m_itemsCountPerIncrement == 0) {
+        m_currentProgress++;
+        if (m_currentProgress <= m_maximumAllowedProgress) {
+            m_realProgressData->setProgressValue(m_currentProgress);
+        }
+    }
+}
+
+int PartialProgressData::getStartingProgress() const {
+    return m_startingProgress;
+}
+
+int PartialProgressData::getMaximumAllowedProgress() const {
+    return m_maximumAllowedProgress;
+}
---
+++ Source/NLBCore/Public/nlb/api/PartialProgressData.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "nlb/api/DummyProgressData.h"
+
+class ProgressData; // Forward declaration
+
+class PartialProgressData : public DummyProgressData {
+private:
+    ProgressData* m_realProgressData;
+    int m_startingProgress;
+    int m_currentProgress;
+    
+    /**
+     * Please note that when current progress is more than maximum progress, specified for progress control
+     * (e.g. >= 100), then process will be considered finished, even in case when it is not.
+     * This can result in prematurely thread termination, so please set maximum allowed progress to value
+     * less than maximum progress, specified for progress control.
+     */
+    int m_maximumAllowedProgress;
+    int m_itemsCountPerIncrement;
+    int m_currentItemsCount;
+
+public:
+    PartialProgressData(
+        ProgressData* realProgressData,
+        int startingProgress,
+        int maximumAllowedProgress,
+        int itemsCountPerIncrement
+    );
+
+    void setRealProgressValue();
+    
+    int getStartingProgress() const;
+    int getMaximumAllowedProgress() const;
+};
